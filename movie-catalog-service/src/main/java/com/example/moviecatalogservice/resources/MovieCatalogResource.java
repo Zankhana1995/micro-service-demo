9c2f1c83f74b00304acbcbe452b9e98b3e146fa9
Zankhana Patel <zankhanapatel1995.zp@gmail.com>
2019-12-05 00:12:06 -0500

    made the bean fro restTemplate

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.example.moviecatalogservice.resources;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
 import com.example.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,10 +15,12 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(String userId) {
-
-        RestTemplate restTemplate = new RestTemplate();
         List<Rating> ratingsList = Arrays.asList(
                 new Rating("1234", 3),
                 new Rating("5678", 4)
