6b98d6493df1fa458c204f6010f058ef859b7407
Zankhana Patel <zankhanapatel1995.zp@gmail.com>
2019-12-04 23:54:42 -0500

    call another microservice through rest template

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,19 +1,34 @@
 package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
+import com.example.moviecatalogservice.models.Movie;
+import com.example.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Transformer", "test", 4)
-    );
+
+        RestTemplate restTemplate = new RestTemplate();
+        List<Rating> ratingsList = Arrays.asList(
+                new Rating("1234", 3),
+                new Rating("5678", 4)
+        );
+
+        return ratingsList.stream()
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
     }
 }
