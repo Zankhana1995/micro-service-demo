347e5029d8b1b552598fb0da7f0c24842f9e690e
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-01-12 20:06:31 -0500

    client side load balancing

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -1,7 +1,9 @@
 package com.example.moviecatalogservice;
 
+import com.netflix.discovery.DiscoveryClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -9,12 +11,16 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 public class MovieCatalogServiceApplication {
+
+    private DiscoveryClient discoveryClient;
+
     public static void main(String[] args) {
 
         SpringApplication.run(MovieCatalogServiceApplication.class, args);
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -25,10 +25,10 @@ public class MovieCatalogResource {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(String userId) {
 
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
         return ratings.getUserRating().stream()
                 .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
                 /*
                     Movie movie = webClientBuilder.build()
                             .get()
